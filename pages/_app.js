4749c619d1d78fdc515158d6fef5ff94b5ba8e5e
astern3 <avram.stern@digitas.com>
2021-01-25 16:57:03 -0600

    initial build, with basic functionality and routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { ThemeProvider } from "theme-ui";
+import theme from "../styles/theme";
+import Nav from "../src/components/nav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Nav />
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
